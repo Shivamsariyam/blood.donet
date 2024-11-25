6ee096659d25fbc651342d02c75f5038e68a14a2
Shivamsariyam <ridergeming61@gmail.com>
2024-11-26 00:47:07 +0530

    update

---
+++ profile.js
@@ -0,0 +1,10 @@
+
+const name =  document.getElementById("name1").innerHTML= localStorage.getItem("name");
+
+const num = document.getElementById("number1").innerHTML= localStorage.getItem("num");
+
+const wsNumber = document.getElementById("wts1").innerHTML= localStorage.getItem("ws-number");
+
+const dist = document.getElementById("dist").innerHTML= localStorage.getItem("district");
+
+const blood = document.getElementById("blood1").innerHTML = localStorage.getItem("blood");
